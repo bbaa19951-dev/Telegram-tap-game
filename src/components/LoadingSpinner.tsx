097be5f143fa3e62eb96194790028084c2b2ec1a
bbaa19951-dev <bbaa19951@gmail.com>
2026-07-09 12:53:29 +0300

    Create LoadingSpinner.tsx

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+// src/components/LoadingSpinner.tsx
+
+export default function LoadingSpinner() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gold"></div>
+    </div>
+  );
+}
